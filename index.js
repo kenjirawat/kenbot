5c2524c8d3822ab6debba707cabb7ccc1933ef07
kenjirawat <jirawat_ken@hotmail.com>
2016-04-24 14:46:10 +0700

    add

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var request = request('request')
 var token = 'CAAWBsWbe93ABAJHtoZCIPGhG5mFmV2ocmZB5BaZBOjYbsqQIK48eMUtuBy6tBxizpw0vnEGxByG9Rc80jCke10gb5L70SYNdJbJmUZBZCl6Re33LSypKLjZCZBC8dmFDa88pAR4cXMjtkCZCJoW6NNcMxMJMJjFKeODLf30UHK3Fzmri8vZCobskPghJvah54A54ZD'
-
-app.use(bodyParser.json())
-app.get('/', function (req, res) {
-  res.send('Hell word')
-})
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === '1234') {
-    res.send(req.query['hub.challenge'])
-  } else {
-    res.send('Error, wrong validation token')
-  }
-})
-app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging
-  for (var i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i]
-    var sender = event.sender.id
-    if (event.message && event.message.text) {
-      var text = event.message.text
-      console.log(text);
-    // Handle a text message from this sender
-    }
-  }
-  res.sendStatus(200)
-})
-
 function sendTextMessage (sender, text) {
   messageData = {
     text: text
@@ -48,6 +23,32 @@ function sendTextMessage (sender, text) {
     }
   })
 }
+
+app.use(bodyParser.json())
+app.get('/', function (req, res) {
+  res.send('Hell word')
+})
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === '1234') {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Error, wrong validation token')
+  }
+})
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
+    if (event.message && event.message.text) {
+      var text = event.message.text
+      console.log(text)
+      sendTextMessage(sender,text)
+    // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200)
+})
 
 app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function () {
