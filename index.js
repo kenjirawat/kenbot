79d94ac8ef2147e6223663e22df6c789f5650c54
kenjirawat <jirawat_ken@hotmail.com>
2016-04-24 13:38:56 +0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hell word')
 })
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 1234) {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Error, wrong validation token')
+  }
+})
 app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function () {
   console.log('Example app listen on port' + app.get('port' + '!'))
